e2f27b4691ff897e3710229e99142ecdfeb7b66a
Maksym <maksymRozhko7@gmail.com>
2022-07-05 18:11:33 +0300

    feat: preloader, header structure

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,4 @@
-// import preloader from './components/preloader';
+import preloader from './components/preloader';
 // import cursor from './components/cursor';
 // import initSliders from './components/initSliders';
 // import headerScrollFixed from './components/fixedHeader';
--- src/js/components/preloader.js
+++ src/js/components/preloader.js
@@ -1,20 +1,14 @@
 function preloader(preloader) {
     const loader = document.querySelector(preloader);
-    const video = document.querySelector('.main-screen__video');
 
     if (loader) {
         setTimeout(() => {
-            loader.children[0].classList.add('active');
+            loader.classList.add('remove');
         }, 1500);
         setTimeout(() => {
-            loader.classList.add('remove');
-        }, 3500);
-        setTimeout(() => {
             loader.remove();
-            video.setAttribute('autoplay', 'autoplay');
-            video.play();
         }, 5500);
     }
 };
 
-preloader('.preloader');
+preloader('.preloader-dots');
