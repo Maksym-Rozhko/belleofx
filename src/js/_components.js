89d40b22fc9a000dca42717a057b446cf95a072f
Maksym <maksymRozhko7@gmail.com>
2022-07-06 14:34:22 +0300

    feat: main screen, adaptive, init main slider

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,6 +1,6 @@
 import preloader from './components/preloader';
 // import cursor from './components/cursor';
-// import initSliders from './components/initSliders';
+import initSliders from './components/initSliders';
 import headerScrollFixed from './components/fixedHeader';
 // import scrollAnimation from './components/scrollAnimation';
 // import tabs from './components/tabs';
