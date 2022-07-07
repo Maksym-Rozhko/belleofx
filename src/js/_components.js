f0e58a8b2184aea4a2e230eaf4f149b350e11c65
Maksym <maksymRozhko7@gmail.com>
2022-07-07 15:15:29 +0300

    feat: new section tips & tricks, init accordion

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,6 +4,6 @@ import initSliders from './components/initSliders';
 import headerScrollFixed from './components/fixedHeader';
 // import scrollAnimation from './components/scrollAnimation';
 // import tabs from './components/tabs';
-// import accordion from './components/accordion';
+import accordion from './components/accordion';
 // import scrollToTop from './components/scrollToTop';
 // import smoothScrollToElems from './components/smoothScrollToElems';
