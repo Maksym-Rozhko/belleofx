fc83e6620ec079c1c552fa3f60dbfe0f1928658f
Maksym <maksymRozhko7@gmail.com>
2022-07-07 22:10:32 +0300

    feat: button scroll top, init scroll

--- src/js/_components.js
+++ src/js/_components.js
@@ -5,5 +5,5 @@ import headerScrollFixed from './components/fixedHeader';
 // import scrollAnimation from './components/scrollAnimation';
 // import tabs from './components/tabs';
 import accordion from './components/accordion';
-// import scrollToTop from './components/scrollToTop';
+import scrollToTop from './components/scrollToTop';
 // import smoothScrollToElems from './components/smoothScrollToElems';
